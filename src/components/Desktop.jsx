8c6bdd1689c8799aa6b8a1c4fa658364219015eb
0xJordan <ahmedjamlan12@gmail.com>
2023-08-07 07:16:33 +0300

    Created Desktop, taskbar and menu components

---
+++ src/components/Desktop.jsx
@@ -0,0 +1,6 @@
+export default function Desktop() {
+  return (
+    <div className="h-[96vh]" style={{backgroundImage: "url('/desktop-wallpaper.jpeg')", backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+    </div>
+  )
+}
---
+++ src/components/Taskbar.jsx
@@ -0,0 +1,9 @@
+export default function Taskbar() {
+  return (
+    <div className="h-[4vh] bg-blue-950">
+        <div className="flex h-full">
+            <button className="bg-sky-400 text-white px-5 hover:bg-sky-600">Start</button>
+        </div>
+    </div>
+  )
+}
